4d9e913f8400a9469841cf44c01fe57a6a4f5f0e
nima-fh <151225313+nima-fh@users.noreply.github.com>
2025-11-13 21:03:59 +0330

    instance for human class

--- test2.py
+++ test2.py
@@ -2,5 +2,15 @@ def power(n):
     return pow(n,3)
 print(power(10))
 class human:
-    def __init__(self):
-        pass
+    def __init__(self,name,family,age,color,gender):
+        self.name=name
+        self.family=family
+        self.age=age
+        self.color=color
+        self.gender=gender
+    
+    def PersonDc(self):
+        print(f"hello im {self.name} {self.family} and im {self.age} years old")
+        
+nima=human("nima","fakhimi","20","white","male")
+nima.PersonDc()
